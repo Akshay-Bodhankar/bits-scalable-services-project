26370975d1b5d0038fae155eefc5ff560b156ef3
Heena-lab <2024tm93226@wilp.bits-pilani.ac.in>
2025-05-07 08:04:11 +0530

    Integrated student service with auth service and changes server to run with other applications.

--- StudentService/index.js
+++ StudentService/index.js
@@ -11,9 +11,8 @@ app.use(express.json());
 app.use('/students', studentRoutes);
 app.use('/students', vaccinationRoutes);
 app.use(errorHandler);
-
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/students', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    app.listen(4001, () => console.log('Student Service running on port 4001'));
+    app.listen(4001, '0.0.0.0', () => console.log('Student Service running on port 4001'));
   })
   .catch(err => console.error(err));
