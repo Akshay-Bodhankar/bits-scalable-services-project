c8e303e36f313997f9092d83c614f8e4aca84147
Heena-lab <2024tm93226@wilp.bits-pilani.ac.in>
2025-05-07 08:40:56 +0530

    Auth service is added

---
+++ auth-service/index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./swagger');
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected to auth-db'))
+    .catch(err => console.log(err));
+
+app.use('/api/auth', authRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+const PORT = process.env.PORT || 4000;
+const HOST = '0.0.0.0';
+
+app.listen(PORT, HOST, () => {
+  console.log(`Auth Service running on http://${HOST}:${PORT}`);
+});
